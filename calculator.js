750900093ae46b02ca52fe4a1c8d7bbff08deaa4
Cedeli <96904745+Cedeli@users.noreply.github.com>
2024-11-29 14:47:41 +0800

    Add basic evaluation

--- calculator.js
+++ calculator.js
@@ -3,6 +3,9 @@ const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.button');
 const input = display.querySelector('input');
 
+let operation = '+';
+let previousNum = 0;
+
 function add(a, b) {
     return a + b;
 }
@@ -22,12 +25,16 @@ function divide(a, b) {
 function operate(a, b, operator) {
     switch (operator) {
         case '+':
+            console.log(`${a} + ${b}`);
             return add(a, b);
         case '-':
+            console.log(`${a} - ${b}`);
             return subtract(a, b);
-        case '*':
+        case 'x':
+            console.log(`${a} * ${b}`);
             return multiply(a, b);
         case '/':
+            console.log(`${a} / ${b}`);
             return divide(a, b);
         default:
             throw new Error('Invalid operator.');
@@ -39,5 +46,20 @@ buttons.forEach((button) => {
         button.addEventListener('click', () => {
             input.value += button.textContent;
         });
+    }
+
+    if (button.classList.contains('operator')) {
+        button.addEventListener('click', () => {
+            operation = button.textContent;
+            previousNum = input.value;
+            input.value = '';
+        });
+    }
+
+    if (button.classList.contains('equals')) {
+        button.addEventListener('click', () => {
+            let ans = operate(Number(previousNum), Number(input.value), operation);
+            input.value = ans;
+        })
     }
 });
